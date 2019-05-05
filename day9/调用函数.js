f76a2b33dc52e817a8b2ae2a8d44e3ab11979196
yaogengzhu <455947455@qq.com>
2019-05-05 10:21:55 +0800

    apply和call用法

--- day9/调用函数.js
+++ day9/调用函数.js
@@ -1,6 +1,16 @@
-function foo(){
-    console.log('hello world ');
-}
-//
-// foo.call();
-foo.apply();
+// function foo(){
+//     console.log('hello world ');
+// }
+// //
+// // foo.call();
+// foo.apply();
+
+
+// 补充 
+let arr = [1,2,3,4,5];
+let max = Math.max.apply(null,arr);
+console.log(max);
+
+// 由于max方式不支持数组，只能将多个参数逐个传入 。
+let max1 = Math.max(2,3,4,5,666,66);
+console.log(max1);
